4c27b5f71c74d78936c1b4e719486281fc6f0635
Jeet Dekivadia <jeet.university@gmail.com>
2024-10-26 13:41:56 -0400

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import HeroSection from '@/components/HeroSection'
+import ContentVerifier from '@/components/ContentVerifier'
+import StatsSection from '@/components/StatsSection'
+import Testimonials from '@/components/Testimonials'
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <ContentVerifier />
+      <StatsSection />
+      <Testimonials />
+    </>
+  )
+}
