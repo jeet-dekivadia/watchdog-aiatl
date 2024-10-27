ea8425d95cd25e9aab9f6905ec2fb4a286d211d8
Jeet Dekivadia <jeet.university@gmail.com>
2024-10-26 21:59:46 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" type="image/png" />
+      </head>
       <body className={inter.className}>
         <div className="min-h-screen flex flex-col">
           <Navigation />
